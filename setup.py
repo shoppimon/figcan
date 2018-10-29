97f4c25bd2a32ebbc8eb07e768d528cc0088a017
Shahar Evron <shahar@shoppimon.com>
2018-10-29 14:35:48 +0200

    Typing is required in Python 2.7

--- setup.py
+++ setup.py
@@ -32,6 +32,8 @@ setup(
     packages=find_packages(),
     long_description=long_desc,
     long_description_content_type='text/markdown',
-    install_requires=[],
+    install_requires=[
+        "typing; python_version < '3.0'"
+    ],
     python_requires='>=2.7.12,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*',
 )
