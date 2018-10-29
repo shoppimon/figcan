4b694842a5e6157d9f97f4965bd2fdf948ce8a9e
Shahar Evron <shahar@shoppimon.com>
2018-10-29 14:23:02 +0200

    Fix python version spec in setup.py

--- setup.py
+++ setup.py
@@ -33,5 +33,5 @@ setup(
     long_description=long_desc,
     long_description_content_type='text/markdown',
     install_requires=[],
-    python_requires='^2.7 || >=3.4',
+    python_requires='>=2.7.12,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*',
 )
