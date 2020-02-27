0c5ca4661e1d03fb5b9b8cf3f2151b5bbb0c8bf8
Shahar Evron <shahar@shoppimon.com>
2020-02-27 18:11:01 +0200

    Drop Python 3.4 support

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
     install_requires=[
         "typing; python_version < '3.0'"
     ],
-    python_requires='>=2.7.12,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*',
+    python_requires='>=2.7.12,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*',
     package_data={"figcan": ["py.typed"]},
     zip_safe=False,
 )
